61bd7e0d482f88c2ffdc5ce0078aa1332bc2c95e
Gerolf Vent <dev@gerolfvent.de>
2025-09-11 23:47:24 +0200

    Refactored iptables manager to use synchronizers and cleaners

--- internal/firewall-manager/iptables-manager.go
+++ internal/firewall-manager/iptables-manager.go
@@ -64,48 +64,39 @@ func (iptm *IPTablesManager) Setup() error {
 			ipsetExcludeDstName = ipsetExcludeDstPrefix + "6"
 		}
 
+		//
+		// Setup exclude dst ipset and rules
+		//
+
+		var excludeCIDRs []net.IPNet
+		for _, cidr := range iptm.excludeDstCIDRs {
+			if (ipt.IsIPv6() && cidr.IP.To4() == nil) || (!ipt.IsIPv6() && cidr.IP.To4() != nil) {
+				excludeCIDRs = append(excludeCIDRs, cidr)
+			}
+		}
+
 		if _, err := iptm.ips.EnsureNetworkSet(ipsetExcludeDstName, ipsetProtocol); err != nil {
 			return fmt.Errorf("failed to ensure %s exclude dst ipset: %w", ipsetProtocol, err)
 		}
 
-		existingExcludeCIDRs, err := iptm.ips.ListNetworkEntries(ipsetExcludeDstName)
+		excludeCIDRsSynchronizer := &IPSetIPNetsSynchronizer{
+			ips:     iptm.ips,
+			SetName: ipsetExcludeDstName,
+			Entries: excludeCIDRs,
+		}
+		err := excludeCIDRsSynchronizer.Sync()
 		if err != nil {
-			return fmt.Errorf("failed to list %s exclude dst ipset entries: %w", ipsetProtocol, err)
-		}
-		for _, existingCIDR := range existingExcludeCIDRs {
-			found := false
-			for _, cidr := range iptm.excludeDstCIDRs {
-				if existingCIDR.String() == cidr.String() {
-					found = true
-					break
-				}
-			}
-			if !found {
-				if _, err := iptm.ips.DeleteNetworkEntry(ipsetExcludeDstName, &existingCIDR); err != nil {
-					return fmt.Errorf("failed to delete stale %s exclude dst ipset entry: %w", ipsetProtocol, err)
-				}
-			}
-		}
-		for _, cidr := range iptm.excludeDstCIDRs {
-			switch ipsetProtocol {
-			case ipset.IPv4:
-				if cidr.IP.To4() == nil {
-					continue
-				}
-			case ipset.IPv6:
-				if cidr.IP.To16() == nil || cidr.IP.To4() != nil {
-					continue
-				}
-			}
-			if _, err := iptm.ips.EnsureNetworkEntry(ipsetExcludeDstName, &cidr); err != nil {
-				return fmt.Errorf("failed to ensure %s exclude dst ipset entry: %w", ipsetProtocol, err)
-			}
+			return fmt.Errorf("failed to sync %s exclude dst ipset entries: %w", ipsetProtocol, err)
 		}
 
 		excludeCIDRsRule := IPTablesExcludeCIDRsRule{
 			IPSetName: ipsetExcludeDstName,
 			Protocol:  ipt.Protocol(),
 		}
+
+		//
+		// Setup mangle chain
+		//
 
 		if _, err := ipt.EnsureChain(iptables.TableMangle, iptablesRTMarkChainName); err != nil {
 			return fmt.Errorf("failed to ensure %s mangle chain: %w", ipt.Protocol(), err)
@@ -119,6 +110,10 @@ func (iptm *IPTablesManager) Setup() error {
 			return fmt.Errorf("failed to ensure %s mangle PREROUTING rule: %w", ipt.Protocol(), err)
 		}
 
+		//
+		// Setup filter chain
+		//
+
 		if _, err := ipt.EnsureChain(iptables.TableFilter, iptablesRejectChainName); err != nil {
 			return fmt.Errorf("failed to ensure %s filter chain: %w", ipt.Protocol(), err)
 		}
@@ -126,6 +121,10 @@ func (iptm *IPTablesManager) Setup() error {
 		if _, err := ipt.EnsureRule(iptables.Prepend, iptables.TableFilter, iptables.ChainForward, "-j", iptablesRejectChainName); err != nil {
 			return fmt.Errorf("failed to ensure %s filter FORWARD rule: %w", ipt.Protocol(), err)
 		}
+
+		//
+		// Setup NAT chain
+		//
 
 		if _, err := ipt.EnsureChain(iptables.TableNAT, iptablesSNATChainName); err != nil {
 			return fmt.Errorf("failed to ensure %s NAT chain: %w", ipt.Protocol(), err)
@@ -214,6 +213,20 @@ func (iptm *IPTablesManager) Cleanup() error {
 				}
 			}
 		}
+
+		ipsetNames, err := iptm.ips.ListSets()
+		if err != nil {
+			errs = append(errs, fmt.Errorf("failed to list ip sets: %w", err))
+		} else {
+			// Cleanup any ip sets that start with METALEG- or inet6:METALEG-
+			for _, ipsetName := range ipsetNames {
+				if strings.HasPrefix(ipsetName, "METALEG-") || strings.HasPrefix(ipsetName, "inet6:METALEG-") {
+					if _, err := iptm.ips.DeleteSet(ipsetName); err != nil {
+						errs = append(errs, fmt.Errorf("failed to delete ip set %q: %w", ipsetName, err))
+					}
+				}
+			}
+		}
 	}
 
 	return errors.Join(errs...)
@@ -249,7 +262,7 @@ func (iptm *IPTablesManager) ReconcileEgressRule(rule *EgressRule, present bool)
 		}
 
 		//
-		// Sync FW ip set (1/3)
+		// Sync rule ip set (1/2)
 		//
 
 		// Ensure the ipset exists if there are any rules to apply, so they don't throw errors,
@@ -261,50 +274,27 @@ func (iptm *IPTablesManager) ReconcileEgressRule(rule *EgressRule, present bool)
 		}
 
 		//
-		// Sync FW Reject rules
+		// Sync Reject rules
 		// These are used to reject traffic, if the gateway node is not local and the
 		// route to the gateway is unknown.
 		//
 
-		// Query all existing reject rules in the filter table
-		rejectRules, err := ipt.ListRules(iptables.TableFilter, iptablesRejectChainName)
+		rejectRule := &IPTablesRejectRule{
+			SrcIPSetName: ipsetSrcName,
+			Protocol:     ipt.Protocol(),
+		}
+
+		rejectRuleSynchronizer := &IPTablesRuleSynchronizer[*IPTablesRejectRule]{
+			ipt:     ipt,
+			Parser:  ParseIPTablesRejectRule,
+			Table:   iptables.TableFilter,
+			Chain:   iptablesRejectChainName,
+			Rule:    rejectRule,
+			Present: present && !isGWLocal && (!isGWRouteKnown || !isGWRouteAllocated),
+		}
+		err := rejectRuleSynchronizer.Sync()
 		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to list iptables rules: %w", err))
-		}
-
-		foundRejectRule := false
-
-		// Cleanup any conflicting or left-over reject rules for the current rule
-		for _, rejectRule := range rejectRules {
-			parsedRejectRule, ok := ParseIPTablesRejectRule(rejectRule[2:], ipt.Protocol())
-			if ok && // Only consider valid reject rules (invalid ones are cleaned up by the full reconciliation)
-				strings.Contains(parsedRejectRule.SrcIPSetName, ruleHash) { // Check if the rule matches the current rule hash
-				// Then remove the rule if:
-				if !present || // The rule is absent
-					isGWLocal || // The gw node is local (because we can SNAT directly)
-					(isGWRouteKnown && isGWRouteAllocated) || // The gw node is known and allocated (because we can route to it)
-					!rule.MatchesIPTablesRejectRule(parsedRejectRule) || // The rule does not match the desired state
-					foundRejectRule { // The rule is a duplicate
-					if _, err := ipt.DeleteRule(iptables.TableFilter, iptablesRejectChainName, rejectRule[2:]...); err != nil {
-						errs = append(errs, fmt.Errorf("failed to delete conflicting iptables rule: %w", err))
-					}
-				} else if rule.MatchesIPTablesRejectRule(parsedRejectRule) {
-					foundRejectRule = true // The rule is present and matches the desired state, so we keep it
-				}
-			}
-		}
-
-		// Create a reject rule, if:
-		if present && // The rule should be present
-			!isGWLocal && (!isGWRouteKnown || !isGWRouteAllocated) && // This node cannot route to the gateway node
-			!foundRejectRule { // No matching reject rule exists
-			rejectRule := IPTablesRejectRule{
-				SrcIPSetName: ipsetSrcName,
-				Protocol:     ipt.Protocol(),
-			}
-			if _, err := ipt.EnsureRule(iptables.Append, iptables.TableFilter, iptablesRejectChainName, rejectRule.Spec()...); err != nil {
-				errs = append(errs, fmt.Errorf("failed to ensure iptables rule: %w", err))
-			}
+			errs = append(errs, err)
 		}
 
 		//
@@ -312,131 +302,68 @@ func (iptm *IPTablesManager) ReconcileEgressRule(rule *EgressRule, present bool)
 		// These are used to mark packets for routing to the gateway node.
 		//
 
-		// Query all existing rt mark rules in the mangle table
-		rtMarkRules, err := ipt.ListRules(iptables.TableMangle, iptablesRTMarkChainName)
+		rtMarkRule := &IPTablesMarkRule{
+			SrcIPSetName: ipsetSrcName,
+			FWMark:       0,
+			FWMask:       uint32(iptm.fwMask),
+			Protocol:     ipt.Protocol(),
+		}
+		if rule.GWRoute != nil {
+			rtMarkRule.FWMark = rule.GWRoute.FWMark
+		}
+
+		rtMarkRuleSynchronizer := &IPTablesRuleSynchronizer[*IPTablesMarkRule]{
+			ipt:     ipt,
+			Parser:  ParseIPTablesMarkRule,
+			Table:   iptables.TableMangle,
+			Chain:   iptablesRTMarkChainName,
+			Rule:    rtMarkRule,
+			Present: present && !isGWLocal && (isGWRouteKnown && isGWRouteAllocated),
+		}
+		err = rtMarkRuleSynchronizer.Sync()
 		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to list iptables rules: %w", err))
-		}
-
-		foundRTMarkRule := false
-
-		// Cleanup any conflicting or left-over rt mark rules for the current rule
-		for _, rtMarkRule := range rtMarkRules {
-			parsedRTMarkRule, ok := ParseIPTablesMarkRule(rtMarkRule[2:], ipt.Protocol())
-			if ok && // Only consider valid rt mark rules (invalid ones are cleaned up by the full reconciliation)
-				strings.Contains(parsedRTMarkRule.SrcIPSetName, ruleHash) { // Check if the rule matches the current rule hash
-				// Then remove the rule if:
-				if !present || // The rule is absent
-					isGWLocal || // The gw node is local (because we can SNAT directly)
-					(!isGWRouteKnown || !isGWRouteAllocated) || // The gw node is unknown or not allocated (because we cannot route to it)
-					!rule.MatchesIPTablesMarkRule(parsedRTMarkRule, uint32(iptm.fwMask)) || // The rule does not match the desired state
-					foundRTMarkRule { // The rule is a duplicate
-					if _, err := ipt.DeleteRule(iptables.TableMangle, iptablesRTMarkChainName, rtMarkRule[2:]...); err != nil {
-						errs = append(errs, fmt.Errorf("failed to delete conflicting iptables rule: %w", err))
-					}
-				} else if rule.MatchesIPTablesMarkRule(parsedRTMarkRule, uint32(iptm.fwMask)) {
-					foundRTMarkRule = true // The rule is present and matches the desired state, so we keep it
-				}
-			}
-		}
-
-		// Create a new rt mark rule, if:
-		if present && // The rule should be present
-			!isGWLocal && (isGWRouteKnown && isGWRouteAllocated) && // This node can route to the gateway node
-			!foundRTMarkRule { // No matching rt mark rule exists
-			rtMarkRule := IPTablesMarkRule{
-				SrcIPSetName: ipsetSrcName,
-				FWMark:       rule.GWRoute.FWMark,
-				FWMask:       uint32(iptm.fwMask),
-				Protocol:     ipt.Protocol(),
-			}
-			if _, err := ipt.EnsureRule(iptables.Append, iptables.TableMangle, iptablesRTMarkChainName, rtMarkRule.Spec()...); err != nil {
-				errs = append(errs, fmt.Errorf("failed to ensure iptables rule: %w", err))
-			}
+			errs = append(errs, err)
 		}
 
 		//
-		// Sync FW SNAT rules
+		// Sync SNAT rules
 		// These are used to SNAT traffic if the local node is the gateway node.
 		//
 
-		// Query all existing SNAT rules in the nat table
-		snatRules, err := ipt.ListRules(iptables.TableNAT, iptablesSNATChainName)
+		snatRule := &IPTablesSNATRule{
+			SrcIPSetName: ipsetSrcName,
+			SNATIP:       snatIP,
+			Protocol:     ipt.Protocol(),
+		}
+
+		snatRuleSynchronizer := &IPTablesRuleSynchronizer[*IPTablesSNATRule]{
+			ipt:     ipt,
+			Parser:  ParseIPTablesSNATRule,
+			Table:   iptables.TableNAT,
+			Chain:   iptablesSNATChainName,
+			Rule:    snatRule,
+			Present: present && isGWLocal,
+		}
+		err = snatRuleSynchronizer.Sync()
 		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to list iptables rules: %w", err))
-		}
-
-		foundSNATRule := false
-
-		// Cleanup any conflicting or left-over snat rules for the current rule
-		for _, snatRule := range snatRules {
-			parsedSNATRule, ok := ParseIPTablesSNATRule(snatRule[2:], ipt.Protocol())
-			if ok && // Only consider valid SNAT rules (invalid ones are cleaned up by the full reconciliation)
-				strings.Contains(parsedSNATRule.SrcIPSetName, ruleHash) { // Check if the rule matches the current rule hash
-				// Then remove the rule if:
-				if !present || // The rule is absent
-					!isGWLocal || // The gw node is not local (because we cannot SNAT directly)
-					!rule.MatchesIPTablesSNATRule(parsedSNATRule) || // The rule does not match the desired state
-					foundSNATRule { // The rule is a duplicate
-					if _, err := ipt.DeleteRule(iptables.TableNAT, iptablesSNATChainName, snatRule[2:]...); err != nil {
-						errs = append(errs, fmt.Errorf("failed to delete conflicting iptables rule: %w", err))
-					}
-				} else if rule.MatchesIPTablesSNATRule(parsedSNATRule) {
-					foundSNATRule = true // The rule is present and matches the desired state, so we keep it
-				}
-			}
-		}
-
-		// Create a new SNAT rule, if:
-		if present && // The rule should be present
-			isGWLocal && // This node is the gateway node (because we can SNAT directly)
-			!foundSNATRule { // No matching SNAT rule exists
-			snatRule := IPTablesSNATRule{
-				SrcIPSetName: ipsetSrcName,
-				SNATIP:       snatIP,
-				Protocol:     ipt.Protocol(),
-			}
-			if _, err := ipt.EnsureRule(iptables.Append, iptables.TableNAT, iptablesSNATChainName, snatRule.Spec()...); err != nil {
-				errs = append(errs, fmt.Errorf("failed to ensure iptables rule: %w", err))
-			}
+			errs = append(errs, err)
 		}
 
 		//
-		// Sync IPSet entries (2/3)
+		// Sync rule ip set (2/2)
 		//
 
-		presentIPs, err := iptm.ips.ListEntries(ipsetSrcName)
+		ipsetSynchronizer := &IPSetIPsSynchronizer{
+			ips:     iptm.ips,
+			SetName: ipsetSrcName,
+			Entries: srcIPs,
+		}
+		err = ipsetSynchronizer.Sync()
 		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to list ipset entries: %w", err))
-		} else {
-			for _, presentSrcIP := range presentIPs {
-				foundSrcIP := false
-				for _, srcIP := range srcIPs {
-					if presentSrcIP.Equal(srcIP) {
-						foundSrcIP = true
-						break
-					}
-				}
-				if !foundSrcIP {
-					// Remove the src IP from the ipset, if it is not present in the rule anymore
-					if _, err := iptm.ips.DeleteEntry(ipsetSrcName, presentSrcIP); err != nil {
-						errs = append(errs, fmt.Errorf("failed to delete ipset entry: %w", err))
-					}
-				}
-			}
-
-			for _, srcIP := range srcIPs {
-				if _, err := iptm.ips.EnsureEntry(ipsetSrcName, srcIP); err != nil {
-					errs = append(errs, fmt.Errorf("failed to ensure ipset entry: %w", err))
-				}
-			}
+			errs = append(errs, err)
 		}
 
-		//
-		// Sync IPSet entries (3/3)
-		//
-
-		// Delete the ipset, if all rules are absent
+		// Delete the ipset, if rule is absent
 		if !present {
 			if _, err := iptm.ips.DeleteSet(ipsetSrcName); err != nil {
 				errs = append(errs, fmt.Errorf("failed to delete ipset: %w", err))
@@ -455,98 +382,69 @@ func (iptm *IPTablesManager) CleanupStaleEgressRules(rules map[string]*EgressRul
 	var errs []error
 
 	for _, ipt := range []iptables.IPTables{iptm.ipt4, iptm.ipt6} {
-		expectedIDHashes := make(set.Set[string], len(rules))
+		expectedRuleIDs := make(set.Set[string], len(rules))
 		for _, rule := range rules {
-			expectedIDHashes.Add(rule.CalcIDHash(ipt.IsIPv6()))
+			expectedRuleIDs.Add(ipsetSrcPrefix + rule.CalcIDHash(ipt.IsIPv6()))
 		}
 
 		//
-		// Cleanup FW Reject rules
+		// Cleanup Reject rules
 		//
 
-		// Query all existing reject rules in the filter table
-		rejectRules, err := ipt.ListRules(iptables.TableFilter, iptablesRejectChainName)
+		rejectRuleCleaner := &IPTablesRuleCleaner[*IPTablesRejectRule]{
+			ipt:             ipt,
+			Parser:          ParseIPTablesRejectRule,
+			Table:           iptables.TableFilter,
+			Chain:           iptablesRejectChainName,
+			ExpectedRuleIDs: expectedRuleIDs,
+			IgnoreRulePredicate: func(ruleSpec []string) bool {
+				_, ok := ParseIPTablesExcludeCIDRsRule(ruleSpec, ipt.Protocol())
+				return ok // Ignore the exclude CIDRs rule
+			},
+		}
+		err := rejectRuleCleaner.Clean()
 		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to list iptables rules: %w", err))
-		}
-
-		// Cleanup any left-over reject rules that do not match any rule ID
-		for _, rejectRule := range rejectRules {
-			parsedRejectRule, ok := ParseIPTablesRejectRule(rejectRule, ipt.Protocol())
-			ruleValid := false
-			if ok {
-				ruleID := strings.TrimPrefix(parsedRejectRule.SrcIPSetName, ipsetSrcPrefix)
-				if expectedIDHashes.Contains(ruleID) {
-					ruleValid = true
-				}
-			}
-			if !ruleValid {
-				if _, err := ipt.DeleteRule(iptables.TableFilter, iptablesRejectChainName, rejectRule[2:]...); err != nil {
-					errs = append(errs, fmt.Errorf("failed to delete unknown iptables rule: %w", err))
-				}
-			}
+			errs = append(errs, err)
 		}
 
 		//
 		// Cleanup RT Mark rules
 		//
 
-		// Query all existing rt mark rules in the mangle table
-		rtMarkRules, err := ipt.ListRules(iptables.TableMangle, iptablesRTMarkChainName)
-		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to list iptables rules: %w", err))
+		rtMarkRuleCleaner := &IPTablesRuleCleaner[*IPTablesMarkRule]{
+			ipt:             ipt,
+			Parser:          ParseIPTablesMarkRule,
+			Table:           iptables.TableMangle,
+			Chain:           iptablesRTMarkChainName,
+			ExpectedRuleIDs: expectedRuleIDs,
+			IgnoreRulePredicate: func(ruleSpec []string) bool {
+				_, ok := ParseIPTablesExcludeCIDRsRule(ruleSpec, ipt.Protocol())
+				return ok // Ignore the exclude CIDRs rule
+			},
 		}
-
-		// Cleanup any left-over rt mark rules that do not match any rule ID
-		for _, rtMarkRule := range rtMarkRules {
-			if _, ok := ParseIPTablesExcludeCIDRsRule(rtMarkRule, ipt.Protocol()); ok {
-				continue // Skip the exclude CIDRs rule
-			}
-
-			parsedRTMarkRule, ok := ParseIPTablesMarkRule(rtMarkRule, ipt.Protocol())
-			ruleValid := false
-			if ok {
-				ruleID := strings.TrimPrefix(parsedRTMarkRule.SrcIPSetName, ipsetSrcPrefix)
-				if expectedIDHashes.Contains(ruleID) {
-					ruleValid = true
-				}
-			}
-			if !ruleValid {
-				if _, err := ipt.DeleteRule(iptables.TableMangle, iptablesRTMarkChainName, rtMarkRule[2:]...); err != nil {
-					errs = append(errs, fmt.Errorf("failed to delete unknown iptables rule: %w", err))
-				}
-			}
+		err = rtMarkRuleCleaner.Clean()
+		if err != nil {
+			errs = append(errs, err)
 		}
 
 		//
-		// Cleanup FW SNAT rules
+		// Cleanup SNAT rules
 		//
 
-		// Query all existing SNAT rules in the nat table
-		snatRules, err := ipt.ListRules(iptables.TableNAT, iptablesSNATChainName)
-		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to list iptables rules: %w", err))
+		snatRuleCleaner := &IPTablesRuleCleaner[*IPTablesSNATRule]{
+			ipt:             ipt,
+			Parser:          ParseIPTablesSNATRule,
+			Table:           iptables.TableNAT,
+			Chain:           iptablesSNATChainName,
+			ExpectedRuleIDs: expectedRuleIDs,
+			IgnoreRulePredicate: func(ruleSpec []string) bool {
+				_, ok := ParseIPTablesExcludeCIDRsRule(ruleSpec, ipt.Protocol())
+				return ok // Ignore the exclude CIDRs rule
+			},
 		}
-
-		// Cleanup any left-over snat rules that do not match any rule ID
-		for _, snatRule := range snatRules {
-			if _, ok := ParseIPTablesExcludeCIDRsRule(snatRule, ipt.Protocol()); ok {
-				continue // Skip the exclude CIDRs rule
-			}
-
-			parsedSNATRule, ok := ParseIPTablesSNATRule(snatRule, ipt.Protocol())
-			ruleValid := false
-			if ok {
-				ruleID := strings.TrimPrefix(parsedSNATRule.SrcIPSetName, ipsetSrcPrefix)
-				if expectedIDHashes.Contains(ruleID) {
-					ruleValid = true
-				}
-			}
-			if !ruleValid {
-				if _, err := ipt.DeleteRule(iptables.TableNAT, iptablesSNATChainName, snatRule[2:]...); err != nil {
-					errs = append(errs, fmt.Errorf("failed to delete unknown iptables rule: %w", err))
-				}
-			}
+		err = snatRuleCleaner.Clean()
+		if err != nil {
+			errs = append(errs, err)
 		}
 	}
 
