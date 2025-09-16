5a932359fc24eb5d161384fcfe60e0dd6816a900
Gerolf Vent <dev@gerolfvent.de>
2025-09-16 14:47:35 +0200

    Cleaned up node route struct

--- internal/route-manager/node-route.go
+++ internal/route-manager/node-route.go
@@ -2,8 +2,6 @@ package route_manager
 
 import (
 	"net"
-
-	"github.com/vishvananda/netlink"
 )
 
 type NodeRoute struct {
@@ -15,60 +13,4 @@ type NodeRoute struct {
 	FWMark       uint32
 	RouteTableID uint32
 	RuleCount    uint // Number of egress rules that use this route
-}
-
-func (r *NodeRoute) MatchesNLRule(nlRule *netlink.Rule) bool {
-	if nlRule == nil {
-		return false
-	}
-
-	if r.RuleCount == 0 {
-		return false // This route is not used by any egress rules
-	}
-
-	if nlRule.Mark != r.FWMark {
-		return false
-	}
-
-	if nlRule.Table != int(r.RouteTableID) {
-		return false
-	}
-
-	return true
-}
-
-func (r *NodeRoute) MatchesNLRoute(nlRoute *netlink.Route) bool {
-	if nlRoute == nil {
-		return false
-	}
-
-	if r.RuleCount == 0 {
-		return false // This route is not used by any egress rules
-	}
-
-	if nlRoute.Table != int(r.RouteTableID) {
-		return false
-	}
-
-	if nlRoute.Dst == nil {
-		return false
-	}
-
-	dstMaskSize, _ := nlRoute.Dst.Mask.Size()
-	if dstMaskSize != 0 { // This should be a default route
-		return false
-	}
-
-	switch nlRoute.Family {
-	case netlink.FAMILY_V4:
-		if r.IPv4 != nil && r.IPv4.Equal(nlRoute.Gw.To4()) {
-			return true
-		}
-	case netlink.FAMILY_V6:
-		if r.IPv6 != nil && r.IPv6.Equal(nlRoute.Gw.To16()) {
-			return true
-		}
-	}
-
-	return false
 }
